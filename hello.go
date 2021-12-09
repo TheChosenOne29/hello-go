0084357aea90d8bb0602be4c4b4dcbc90c0dcccb
TheChosenOne29 <gilbertwuaten@gmail.com>
2021-12-10 00:33:14 +0700

    Added features

--- hello.go
+++ hello.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func hola(a, b string) {
+	fmt.Println("\nHello, I'm " + a + " " + b)
+}
 func main() {
-	fmt.Println("Hello World")
+	var name string
+	var nameLast string
+
+	fmt.Println("Hello World!")
+
+	fmt.Print("My first name is ")
+	fmt.Scan(&name)
+
+	fmt.Print("and my last name is ")
+	fmt.Scan(&nameLast)
+
+	hola(name, nameLast)
 }
